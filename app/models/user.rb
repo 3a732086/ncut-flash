39551262d3c218f2d24a3408591a0d733008afb2
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-07-10 06:31:10 +0800

    開啟 devise trackable 功能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable
+         :confirmable, :trackable
 
   validates :name, :student_class, :role, presence: true
   enum role: [:guest, :student, :admin]
