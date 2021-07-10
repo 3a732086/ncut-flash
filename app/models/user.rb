6ec123508d9edd183c50708dcefd571abaaf634e
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-07-10 23:11:18 +0800

    註解掉程式碼，否則會報錯

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,10 @@ class User < ApplicationRecord
 
 
   protected
-
-  def serializable_hash(options = nil)
-    super(options).merge(current_sign_in_at: current_sign_in_at, last_sign_in_at: last_sign_in_at) # you can keep adding attributes here that you wish to expose
-  end
+  # 這段程式碼的用意是在資料表可以看見current_sign_in_at這些欄位，但是在 rails console裡面看不到
+  # 所以加上這段程式碼可以在 rails console 裡面看到這些欄位
+  # 但是真正在執行的時候必須要註解掉，不然會報錯
+  # def serializable_hash(options = nil)
+  #   super(options).merge(current_sign_in_at: current_sign_in_at, last_sign_in_at: last_sign_in_at) # you can keep adding attributes here that you wish to expose
+  # end
 end
