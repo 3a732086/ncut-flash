525787d5620c4a316c42c63d9e2c7043ec1474a4
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-07-11 00:21:04 +0800

    controller 名稱要複數，所以刪掉重新產生

--- test/controllers/flashes_controller_test.rb
+++ test/controllers/flashes_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class FlashControllerTest < ActionDispatch::IntegrationTest
+class FlashesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
