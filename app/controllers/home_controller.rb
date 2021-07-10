5d78a7494333b9faf0e659a52c363012191db58c
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-07-11 00:01:53 +0800

    排版

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,8 @@ class HomeController < ApplicationController
     @users = User.count         #註冊的會員數量
     @courses = Course.count     #課程數量
     @user_info = UserInfo.count #今日練習量
-    
+
     log_in_user = User.where(current_sign_in_at: Date.current.to_time.all_day)  #搜尋 User登入時間是否符合今天日期
     @today_log_in = log_in_user.count  #計算今天登入的 User數量
-
-
-
   end
 end
