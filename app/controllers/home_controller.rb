eb7bffa70165fb55c6c5e3eacb8ab4cd49607f2f
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-07-10 23:39:44 +0800

    針對日期更簡潔的寫法

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,8 @@ class HomeController < ApplicationController
     @users = User.count         #註冊的會員數量
     @courses = Course.count     #課程數量
     @user_info = UserInfo.count #今日練習量
-
-    time = Time.now  #抓目前的時間
-    log_in_user = User.where(current_sign_in_at: time.all_day)  #搜尋 User登入時間是否符合今天日期
+    
+    log_in_user = User.where(current_sign_in_at: Date.current.to_time.all_day)  #搜尋 User登入時間是否符合今天日期
     @today_log_in = log_in_user.count  #計算今天登入的 User數量
 
 
