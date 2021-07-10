71a89346536fbcdb7877205daf38ec6f3f3031c0
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-07-10 23:33:41 +0800

    首頁顯示 總會員數量、課程數量、今日練習量、今日登入人數的方法

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
 
   def index
-    
+    @users = User.count         #註冊的會員數量
+    @courses = Course.count     #課程數量
+    @user_info = UserInfo.count #今日練習量
+
+    time = Time.now  #抓目前的時間
+    log_in_user = User.where(current_sign_in_at: time.all_day)  #搜尋 User登入時間是否符合今天日期
+    @today_log_in = log_in_user.count  #計算今天登入的 User數量
+
+
+
   end
 end
