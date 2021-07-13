004ca507c23fcedc466b718ff5aef717096655e4
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-07-13 21:55:53 +0800

    count 改成 全域 @count

--- app/controllers/flashes_controller.rb
+++ app/controllers/flashes_controller.rb
@@ -7,10 +7,10 @@ class FlashesController < ApplicationController
   end
 
   def show
-    course = Course.find_by_id(params[:id])
-    @course_topics = CourseTopic.where(:course_id => course).order("id")
+    @course = Course.find_by_id(params[:id])
+    @course_topics = CourseTopic.where(:course_id => @course).order("id")
 
-    count = @course_topics.count  #總共有幾個問題
+    @count = @course_topics.count  #總共有幾個問題
 
     # 如果網址沒有page的話，他會顯示最後一個問題，這樣是錯的，應該是顯示第一個問題，所以要加判斷
     if params[:page]
@@ -20,9 +20,12 @@ class FlashesController < ApplicationController
     end
 
     @first_page = 1
-    @last_page = ( count / LIMITED_QUESTION_NUMBER )
+    @last_page = ( @count / LIMITED_QUESTION_NUMBER )
 
     @course_topics = @course_topics[ (@page - 1) * LIMITED_QUESTION_NUMBER, LIMITED_QUESTION_NUMBER]
+
+
+    @user_info = UserInfo.new
   end
 
 
