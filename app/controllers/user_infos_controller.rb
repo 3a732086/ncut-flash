f638e5d3c78dfb78c9e31ff63bcc1425908dc11f
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-07-13 21:56:19 +0800

    user_infos controller 修改

--- app/controllers/user_infos_controller.rb
+++ app/controllers/user_infos_controller.rb
@@ -13,10 +13,19 @@ class UserInfosController < ApplicationController
     if !@user_info.valid?
       flash[:alert] = "ERROR!!"
     else
-      flash[:notice] = "提交成功!!"
+      flash[:notice] = "第#{params[:user_info][:topic]}題繳交成功!!"
     end
 
-    redirect_to "http://localhost:3000/flashes/2?page=3"
+
+    count = params[:user_info][:count].to_i   #用表單從flashes#show傳過來的題目總數量
+
+    url_topic_id = params[:user_info][:topic].to_i + 1  #繳交題目後跳至下一題
+
+    if url_topic_id > count   #如果題目是最後一題的話，就不能再 +1
+        url_topic_id -= 1
+    end
+
+    redirect_to "http://localhost:3000/flashes/#{params[:user_info][:scenes_id]}?page=#{url_topic_id}"
 
   end
 
