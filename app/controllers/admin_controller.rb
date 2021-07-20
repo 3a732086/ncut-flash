a5d8fb368bd094a0728fb166fd75fd96b6f5b428
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-07-21 00:40:22 +0800

    完成 admin/index

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,12 @@ class AdminController < ApplicationController
   layout 'admin/layouts/admin'
 
   def index
+    @recent_login_students = User.order(current_sign_in_at: :desc)
 
+    log_in_user = User.where(current_sign_in_at: Date.current.to_time.all_day)  #搜尋 User登入時間是否符合今天日期
+    @today_log_in_user = log_in_user.count  #計算今天登入的 User數量
+
+    practice = UserInfo.where(inserted_at: Date.current.to_time.all_day)   #搜尋當天日期的練習量
+    @today_practice_count = practice.count   #今日練習量
   end
 end
