dd514f50e88157a12562356c808d3109d3f9acd9
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-07-21 14:13:48 +0800

    修改程式碼

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,7 +5,7 @@ class AdminController < ApplicationController
     @recent_log_in_users = User.order(current_sign_in_at: :desc)
 
     log_in_user = User.where(current_sign_in_at: Date.current.to_time.all_day)  #搜尋 User登入時間是否符合今天日期
-    @today_log_in_user = log_in_user.count  #計算今天登入的 User數量
+    @today_log_in_users = log_in_user.count  #計算今天登入的 User數量
 
     practice = UserInfo.where(inserted_at: Date.current.to_time.all_day)   #搜尋當天日期的練習量
     @today_practice_count = practice.count   #今日練習量
