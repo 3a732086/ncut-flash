4a7680a922a8198e5daba1ce524b1d0b24581866
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-07-21 14:05:06 +0800

    修改程式碼，以及 controller 定義 user_manage方法

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   layout 'admin/layouts/admin'
 
   def index
-    @recent_log_in_students = User.order(current_sign_in_at: :desc)
+    @recent_log_in_users = User.order(current_sign_in_at: :desc)
 
     log_in_user = User.where(current_sign_in_at: Date.current.to_time.all_day)  #搜尋 User登入時間是否符合今天日期
     @today_log_in_user = log_in_user.count  #計算今天登入的 User數量
@@ -10,4 +10,10 @@ class AdminController < ApplicationController
     practice = UserInfo.where(inserted_at: Date.current.to_time.all_day)   #搜尋當天日期的練習量
     @today_practice_count = practice.count   #今日練習量
   end
+
+
+  def user_manage
+    @users = User.all
+  end
+
 end
