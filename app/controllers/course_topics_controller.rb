a85045bb59177aec7db03d87977928b43b3052dc
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-07-25 16:39:33 +0800

    用 cmd 執行 rails g controller courses, course_topics

---
+++ app/controllers/course_topics_controller.rb
@@ -0,0 +1,2 @@
+class CourseTopicsController < ApplicationController
+end
---
+++ test/controllers/course_topics_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CourseTopicsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
