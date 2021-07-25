0da846eaab27ae468b851544ac3bcd8bd6733997
DESKTOP-5A9RQL9\Win-PC <benabcd5779@gmail.com>
2021-07-25 23:23:58 +0800

    完成後台 courses 頁面

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,40 @@
 class CoursesController < ApplicationController
-  layout 'admin/layouts/admin'
-  
-  def index
+    layout 'admin/layouts/admin'
 
-  end
+    def index
+        @courses = Course.is_published #搜尋 :status = 1 的情境
+
+        @deleted_courses = Course.is_hidden  #搜尋 :status = 0 的情境
+    end
+
+    def create
+        course = Course.create({
+                                   name: params[:name],
+                                   inserted_at: Time.zone.now,
+                               })
+        if course.valid?
+            FileUtils.mkdir_p "#{Rails.public_path}/data/FOLDER_#{course.name}"
+            redirect_to action: :index
+            return
+        end
+    end
+
+    def update
+        course = Course.find(params[:id])
+        course.update({
+                          status: 1  #將 status 設回1
+                      })
+        redirect_to action: :index
+        return
+    end
+
+    def destroy
+        course = Course.find(params[:id])
+        course.update({
+                           status: 0    #刪除並不是真的刪掉，而是把 status 改成0
+                       })
+        redirect_to action: :index
+        return
+    end
+
 end
